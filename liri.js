56d15ba9e4c9af3840b8177ee4f3d8f6f9cf43b4
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-17 17:04:40 -0700

    I put in a couple of variables that will hold the command and then the name of the parameter the user is looking for.

--- liri.js
+++ liri.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
 
 // variable for the Spotify keys
-let keys = require("./keys.js")
+let keys = require("./keys.js");
+const axios = require("axios");
 
 // spotify keys
 console.log(keys);
 console.log(keys.spotify);
+
+let command = process.argv[2];
+let search = process.argv.slice(3).join("+");
+
+console.log(command)
+console.log(search);
