3fc8627054d5fef71b41026d8d9f26e54492dd4c
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-17 18:05:46 -0700

    Finished writing the concert function. Now the program will display the concerts the artist will be performing.

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const moment = require("moment");
 
 // variable for the Spotify keys
 let keys = require("./keys.js");
@@ -10,6 +11,34 @@ console.log(keys.spotify);
 
 let command = process.argv[2];
 let search = process.argv.slice(3).join("+");
+let searchDisplay = process.argv.slice(3).join(" ");
 
 console.log(command)
 console.log(search);
+
+// functions for the search
+concert = (artist) => {
+    let queryURL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
+
+    // axios call
+    axios.get(queryURL).then(function (response) {
+        let results = response.data;
+        for (let i = 0; i < results.length; i++) {
+            let date = moment(results[i].datetime).format("MM/DD/YYYY hh:mm A")
+            console.log(`Name: ${results[i].venue.name}`);
+            console.log(`Location: ${results[i].venue.city}`);
+            console.log(`Date: ${date}`);
+            console.log(`--------------------`)
+        }
+    }).catch(error => {
+        if (error.response) {
+            console.log(error.response);
+        }
+    })
+}
+
+// what to run when the user types
+if (command === "concert-this") {
+    console.log(`I will show you concerts for ${searchDisplay}\n`);
+    concert(search);
+}
